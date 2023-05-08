34d4235dffb02423d8c035d0c66130c747536712
Ali Reda <socialstarservices1@gmail.com>
2023-05-08 22:53:08 +0100

    djfsdkljf

--- routes/signin.js
+++ routes/signin.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'h1n1a1y1',
-  database: 'GPDatabase'
+var sqlite3 = require('sqlite3').verbose();
+
+let vaccinedb = new sqlite3.Database('vaccines.db', (err) => {
+  if(err){
+    console.error(err.message);
+  }
+  console.log("connected to GP database")
 })
-
-
-
+let gpdb = new sqlite3.Database('tryingout.db', (err) => {
+if(err){
+    console.error(err.message);
+  }
+  console.log("connected to GP database")
+  
+}) 
 
 
 /* GET users listing. */
@@ -19,7 +24,8 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
-  const {Nhsnumber, firstName, secondName, postcode} = req.body
+  const {email, password} = req.body
+  console.log(email, password)
   if(Nhsnumber===""){
     //do query with others
     
